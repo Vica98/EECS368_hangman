dbdefd75507ccb9d1ba227018b6742964d979534
Victoria Maldonado <vica.chevchenco@ku.edu>
2019-11-06 16:31:21 -0600

    Added ability to show guessed values in the word

--- hangman.js
+++ hangman.js
@@ -1,45 +1,64 @@
-var m_guessed;
-var m_word;
-var m_output;
-function saveInput()
+
+//function start all over when called, figure out how to save data...maybe call change output with string and return changed string
+var secretWord;
+function startGame(firstClick)
 {
-m_word = "hello";
-m_guessed = document.getElementById('guess').value;
-m_output = document.getElementById("out");
-for(let i=0;i<m_word.length;i++)
-{m_output.value += "-"};
-compareInput();
-}
-
-function compareInput()
-{
-  let foundLetter = false;
-  for(let i=0;i<m_word.length;i++){
-
-    if(m_guessed==m_word.charAt(i)){
-      foundLetter = true;
-      console.log(foundLetter);
-      console.log(m_word.charAt(i));
-      break;
-    }else{
-      foundLetter = false;
-      console.log(foundLetter);
-    }
-  }
-if(foundLetter){
-changeOutput();
-}else{
-
-}
-}
-
-function changeOutput()
-{
-  for(let i=0;i<m_word.length;i++)
+  if(firstClick==true)
   {
-    if(m_word.charAt(i)==m_guessed)
-    {
-      m_output = setCharAt(m_output.value,i, m_guessed);
-    }
+    console.log("hi");
+    var theWord = createGame();
+    secretWord=theWord;
+  }
+  var found_it = compareInput(secretWord);
+  if(found_it==true){
+  changeOutput();
   }
 }
+    createGame = () =>{
+    m_word = "hello";
+    console.log("did it");
+    m_output = document.getElementById("out");
+    m_output.value="";
+    for(let i=0;i<m_word.length;i++)
+    {m_output.value += "-"};
+    console.log(m_output.value);
+    return m_word;
+    }
+
+    compareInput = (m_word) =>{
+      console.log(m_output.value);
+      console.log(m_word);
+      m_output = document.getElementById("out");
+      m_guessed =document.getElementById("guess").value;
+      console.log(m_guessed);
+      let foundLetter = false;
+      for(let i=0;i<m_word.length;i++){
+        if(m_guessed==m_word.charAt(i)){
+          foundLetter = true;
+          console.log(foundLetter);
+          console.log(m_word.charAt(i));
+          break;
+        }else{
+          foundLetter = false;
+          console.log(foundLetter);
+        }
+      }
+    return foundLetter;
+    }
+
+    changeOutput = () => {
+    console.log(m_output.value);
+    m_output.value= document.getElementById("out").value;
+    console.log(m_output.value);
+      var left_side_word;
+      var right_side_word;
+      for(let i=0;i<m_word.length;i++)
+      {
+        if(m_word.charAt(i)==m_guessed)
+        {
+          console.log(i);
+          m_output.value= m_output.value.substr(0,i) + m_guessed + m_output.value.substr(i+1);
+        }
+      }
+      console.log(m_output.value);
+    }
